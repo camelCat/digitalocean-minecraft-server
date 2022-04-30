2c78489a4ffdd3ec96636d8acec8e34cb995ceee
Aleksandrs Rimlins <alriml@ttu.ee>
2022-04-30 16:59:28 +0300

    Destroy doesn't work if already destroyed

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+source ~/.bashrc
+if [ -z "$MC_SERVER_IP" ]; 
+then echo "The server doesn't exist"; exit; fi
 ssh root@${MC_SERVER_IP} sh /mc-server/stop.sh
 rsync -avz \
     --exclude '*.sh' \
@@ -10,4 +13,9 @@ rsync -avz \
     --exclude 'eula.txt' \
     root@${MC_SERVER_IP}:/mc-server/* ./backup
 terraform -chdir=terraform destroy --auto-approve
+bash_line="export MC_SERVER_IP="
+sed -i "/$bash_line/d" ~/.bashrc
+bash_line="alias mc="
+sed -i "/$bash_line/d" ~/.bashrc
 rm terraform/.terraform.tfstate* > /dev/null
+rm server-info.txt
